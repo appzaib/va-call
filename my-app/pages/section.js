21dc070411613c8ac5c8a70f8e5a4e19ecb4549c
abdullah-k <abdullah771.ak@gmail.com>
2023-01-26 23:33:29 +0500

    add section

--- my-app/pages/section.js
+++ my-app/pages/section.js
@@ -8,22 +8,160 @@ export default function Section(){
       <>
         <div className="bg-sky-700">
         <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center ">
-    <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-      <h1 class="sm:text-4xl text-8xl mb-4 font-bold text-white">Connecting World for Better Solving.</h1>
-      <p class="mb-8 leading-relaxed font-bold">Improve customer experience by listening and acting on their feedback</p>
+    <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center">
+      <h1 class="md:text-4xl lg:text-8xl mb-4 font-semibold text-white">Connecting World for Better Solving.</h1>
+      <p class="mb-8 leading-relaxed text-white font-bold py-6">Improve customer experience by listening and acting on expert's feedback</p>
       <div class="flex justify-center">
-        <button class="inline-flex text-white bg-yellow-500 border-0 py-2 px-6 focus:outline-none hover:bg-yellow-600 rounded text-lg">Start Free Trial<FaArrowRight className="mt-1 mx-1 "/></button>
+        <button class="inline-flex text-white bg-sky-500 border-0 py-3 px-6 focus:outline-none hover:bg-transparent hover:transition-transform  rounded-xl text-lg">Start Free Trial<FaArrowRight className="mt-1 mx-1 "/></button>
       </div>
     </div>
-    <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+    <div class="lg:max-w-lg lg:w-full w-5/6">
       <img class="object-cover object-center rounded" alt="Img" src="https://preview.moxcreative.com/vocall/wp-content/uploads/sites/7/2022/02/img_9.png"/>
     </div>
   </div>
         </div>
-        <div className="">
-          <div></div>
-          <div></div>
+        <div className="flex flex-wrap container py-20 lg:px-20 md:px-0">
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 ">
+            <img
+            className="object-cover object-center rounded "
+             src="https://preview.moxcreative.com/vocall/wp-content/uploads/sites/7/2022/02/call-center-5.jpg" alt="Img" />
+          </div>
+          <div className="lg:ml-10 lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-wrap md:items-start md:text-left mb-16 md:mb-0 items-center px-10">
+            <p className="leading-relaxed uppercase font-medium text-amber-600">who we are</p>
+            <h1 class="sm:text-2xl lg:text-5xl mb-4 font-bold text-sky-900">We communicate to get your business to the next level.</h1>
+            <p className="lg:text-lg md:text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, hic voluptates quis laborum et repudiandae.</p>
+          <button class="inline-flex text-white bg-sky-500 border-0 py-3 px-6 focus:outline-none hover:bg-sky-900 hover:{transition-dely: 500ms} rounded-full font-semibold text-lg">Discover More<FaArrowRight className="mt-1 mx-1 "/></button>
+
+          </div>
         </div>
+            <h1 className="font-bold text-4xl pb-20 text-center text-sky-900">" We are trusted by over 5000 companies."</h1>
+            <div className="bg-sky-900 pt-20">
+              <div className="lg:ml-10 text-center md:items-start mb-16 md:mb-0 items-center px-10">
+            <p className="leading-relaxed uppercase text-amber-600 font-medium">what we offer</p>
+            <h1 class="sm:text-2xl lg:text-5xl mb-4 font-bold text-white">We will help your <br /> business grow.</h1>
+
+            {/* <div> */}
+ 
+            {/* </div> */}
+
+            
+  <div>
+  <div class="container px-5 py-10 mx-auto  text-center">
+    <div class="flex flex-wrap -m-4">
+      <div class="p-4 md:w-1/3">
+        <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col my-5">
+          <div class="flex items-center mb-3">
+            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+              </svg>
+            </div>
+            <h2 class="text-gray-900 text-lg title-font font-medium">Shooting Stars</h2>
+          </div>
+          <div class="flex-grow">
+            <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 md:w-1/3">
+        <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+          <div class="flex items-center mb-3">
+            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                <circle cx="12" cy="7" r="4"></circle>
+              </svg>
+            </div>
+            <h2 class="text-gray-900 text-lg title-font font-medium">The Catalyzer</h2>
+          </div>
+          <div class="flex-grow">
+            <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 md:w-1/3">
+        <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+          <div class="flex items-center mb-3">
+            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                <circle cx="6" cy="6" r="3"></circle>
+                <circle cx="6" cy="18" r="3"></circle>
+                <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+              </svg>
+            </div>
+            <h2 class="text-gray-900 text-lg title-font font-medium">Neptune</h2>
+          </div>
+          <div class="flex-grow">
+            <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+  <div>
+  <div class="container px-5 py-24 mx-auto  text-center">
+    <div class="flex flex-wrap -m-4">
+      <div class="p-4 md:w-1/3">
+        <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+          <div class="flex items-center mb-3">
+            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+              </svg>
+            </div>
+            <h2 class="text-gray-900 text-lg title-font font-medium">Shooting Stars</h2>
+          </div>
+          <div class="flex-grow">
+            <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 md:w-1/3">
+        <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+          <div class="flex items-center mb-3">
+            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                <circle cx="12" cy="7" r="4"></circle>
+              </svg>
+            </div>
+            <h2 class="text-gray-900 text-lg title-font font-medium">The Catalyzer</h2>
+          </div>
+          <div class="flex-grow">
+            <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
+          </div>
+        </div>
+      </div>
+      <div class="p-4 md:w-1/3">
+        <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+          <div class="flex items-center mb-3">
+            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
+              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                <circle cx="6" cy="6" r="3"></circle>
+                <circle cx="6" cy="18" r="3"></circle>
+                <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
+              </svg>
+            </div>
+            <h2 class="text-gray-900 text-lg title-font font-medium">Neptune</h2>
+          </div>
+          <div class="flex-grow">
+            <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  </div>
+
+
+
+          </div>
+          
+              </div>
         </>
     )
 }
