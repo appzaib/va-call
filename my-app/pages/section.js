3cfc7ea4c55ab35ac91d99a6904f186d7ee42b24
abdullah-k <abdullah771.ak@gmail.com>
2023-01-27 21:59:57 +0500

    add section

--- my-app/pages/section.js
+++ my-app/pages/section.js
@@ -1,4 +1,4 @@
-import { FaArrowRight } from "react-icons/fa"
+import { FaArrowRight, FaComment, FaHeadset, FaMoneyBillAlt, FaPhoneSlash, FaRobot, FaUserSecret, FaCheck, FaDollarSign, FaBalanceScaleRight, FaBars } from "react-icons/fa"
 
 
 
@@ -46,54 +46,39 @@ export default function Section(){
 
             
   <div>
-  <div class="container px-5 py-10 mx-auto  text-center">
+  <div class="container px-5 py-5 mx-auto  text-center">
     <div class="flex flex-wrap -m-4">
       <div class="p-4 md:w-1/3">
-        <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col my-5">
+        <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
           <div class="flex items-center mb-3">
-            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-              </svg>
-            </div>
-            <h2 class="text-gray-900 text-lg title-font font-medium">Shooting Stars</h2>
+              <img className="w-20 mx-3"
+              src="https://themes.webdevia.com/digitalo-creative-digital-agency-wordpress-theme/wp-content/uploads/2019/03/Message-App.svg" alt="Img" />
+            <h2 class="text-sky-900 text-2xl font-medium">Call Center</h2>
           </div>
           <div class="flex-grow">
-            <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
+            <p class="leading-relaxed text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, veritatis! Facilis nihil asperiores optio fugit, illo quidem dolores officia repellendus dolore, omnis sed nulla provident magnam nam sapiente delectus! Dolorem?</p>
+          </div>
+        </div>
+      </div>
+     <div class="p-4 md:w-1/3">
+        <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+          <div class="flex items-center mb-3">
+              <FaMoneyBillAlt className="text-amber-400 mr-3" size={"100px"}/>
+            <h2 class="text-sky-900 text-2xl font-medium">Debt Collection</h2>
+          </div>
+          <div class="flex-grow">
+            <p class="leading-relaxed text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, veritatis! Facilis nihil asperiores optio fugit, illo quidem dolores officia repellendus dolore, omnis sed nulla provident magnam nam sapiente delectus! Dolorem?</p>
           </div>
         </div>
       </div>
       <div class="p-4 md:w-1/3">
         <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
           <div class="flex items-center mb-3">
-            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                <circle cx="12" cy="7" r="4"></circle>
-              </svg>
-            </div>
-            <h2 class="text-gray-900 text-lg title-font font-medium">The Catalyzer</h2>
+            <FaPhoneSlash className="text-sky-400 mr-3  w-24 h-24" />
+            <h2 class="text-sky-900 text-2xl font-medium">Inbound Requests</h2>
           </div>
           <div class="flex-grow">
-            <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
-          </div>
-        </div>
-      </div>
-      <div class="p-4 md:w-1/3">
-        <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
-          <div class="flex items-center mb-3">
-            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                <circle cx="6" cy="6" r="3"></circle>
-                <circle cx="6" cy="18" r="3"></circle>
-                <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
-              </svg>
-            </div>
-            <h2 class="text-gray-900 text-lg title-font font-medium">Neptune</h2>
-          </div>
-          <div class="flex-grow">
-            <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
-            
+            <p class="leading-relaxed text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, veritatis! Facilis nihil asperiores optio fugit, illo quidem dolores officia repellendus dolore, omnis sed nulla provident magnam nam sapiente delectus! Dolorem?</p>
           </div>
         </div>
       </div>
@@ -101,54 +86,38 @@ export default function Section(){
   </div>
   </div>
   <div>
-  <div class="container px-5 py-24 mx-auto  text-center">
+  <div class="container px-5 py-5 mx-auto  text-center">
     <div class="flex flex-wrap -m-4">
       <div class="p-4 md:w-1/3">
         <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
           <div class="flex items-center mb-3">
-            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-              </svg>
-            </div>
-            <h2 class="text-gray-900 text-lg title-font font-medium">Shooting Stars</h2>
+            <FaHeadset className="text-lime-400 mr-3  w-24 h-24"/> 
+            <h2 class="text-sky-900 text-2xl font-medium">Customer Support</h2>
           </div>
           <div class="flex-grow">
-            <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
+            <p class="leading-relaxed text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, veritatis! Facilis nihil asperiores optio fugit, illo quidem dolores officia repellendus dolore, omnis sed nulla provident magnam nam sapiente delectus! Dolorem?</p>
+          </div>
+        </div>
+      </div>
+     <div class="p-4 md:w-1/3">
+        <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
+          <div class="flex items-center mb-3">
+              <FaRobot className="text-fuchsia-400 mr-3" size={"100px"}/>
+            <h2 class="text-sky-900 text-2xl font-medium">Virtual Assistance</h2>
+          </div>
+          <div class="flex-grow">
+            <p class="leading-relaxed text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, veritatis! Facilis nihil asperiores optio fugit, illo quidem dolores officia repellendus dolore, omnis sed nulla provident magnam nam sapiente delectus! Dolorem?</p>
           </div>
         </div>
       </div>
       <div class="p-4 md:w-1/3">
         <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
           <div class="flex items-center mb-3">
-            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-                <circle cx="12" cy="7" r="4"></circle>
-              </svg>
-            </div>
-            <h2 class="text-gray-900 text-lg title-font font-medium">The Catalyzer</h2>
+            <FaComment className="text-indigo-400 mr-3  w-24 h-24" />
+            <h2 class="text-sky-900 text-2xl font-medium">Live Chat Support</h2>
           </div>
           <div class="flex-grow">
-            <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
-          </div>
-        </div>
-      </div>
-      <div class="p-4 md:w-1/3">
-        <div class="flex rounded-lg h-full bg-gray-100 p-8 flex-col">
-          <div class="flex items-center mb-3">
-            <div class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full bg-indigo-500 text-white flex-shrink-0">
-              <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                <circle cx="6" cy="6" r="3"></circle>
-                <circle cx="6" cy="18" r="3"></circle>
-                <path d="M20 4L8.12 15.88M14.47 14.48L20 20M8.12 8.12L12 12"></path>
-              </svg>
-            </div>
-            <h2 class="text-gray-900 text-lg title-font font-medium">Neptune</h2>
-          </div>
-          <div class="flex-grow">
-            <p class="leading-relaxed text-base">Blue bottle crucifix vinyl post-ironic four dollar toast vegan taxidermy. Gastropub indxgo juice poutine.</p>
-            
+            <p class="leading-relaxed text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, veritatis! Facilis nihil asperiores optio fugit, illo quidem dolores officia repellendus dolore, omnis sed nulla provident magnam nam sapiente delectus! Dolorem?</p>
           </div>
         </div>
       </div>
@@ -159,9 +128,81 @@ export default function Section(){
 
 
 
+
           </div>
           
               </div>
+   <div className="flex flex-wrap container py-20 lg:px-20 md:px-0">
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 ">
+            <img
+            className="object-cover object-center rounded "
+             src="https://preview.moxcreative.com/vocall/wp-content/uploads/sites/7/2022/02/img_7.png" alt="Img" />
+          </div>
+          <div className="lg:ml-10 lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-wrap md:items-start md:text-left mb-16 md:mb-0 items-center px-10">
+            <p className="leading-relaxed uppercase font-medium text-amber-600">who we are</p>
+            <h1 class="sm:text-2xl lg:text-5xl mb-4 font-bold text-sky-900">We communicate to get your business to the next level.</h1>
+            <div className="flex flex-wrap">
+              <div className="w-1/2">
+                <h1 className="text-xl font-semibold m-3"><FaCheck className="mr-2 inline-flex rounded-lg p-1 bg-indigo-400 text-white"/>Professional Staff</h1>
+                <p className="m-3">We possess specialized knowledge, skills, and experience in a particular field to provide services or support to your company.</p>
+              </div>
+              <div className="w-1/2">
+                <h1 className="text-xl font-semibold m-3"><FaCheck className="mr-2 inline-flex rounded-lg p-1 bg-indigo-400 text-white"/>Affordable Price</h1>
+                <p className="m-3">We possess specialized knowledge, skills, and experience in a particular field to provide services or support to your company.</p>
+              </div>
+
+            </div>
+            <div className="flex flex-wrap">
+              <div className="w-1/2">
+                <h1 className="text-xl font-semibold m-3"><FaCheck className="mr-2 inline-flex rounded-lg p-1 bg-indigo-400 text-white"/>100% Guarantee Satisfaction</h1>
+                <p className="m-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus commodi .</p>
+              </div>
+              <div className="w-1/2">
+                <h1 className="text-xl font-semibold m-3"><FaCheck className="mr-2 inline-flex rounded-lg p-1 bg-indigo-400 text-white"/>24/7 Support</h1>
+                <p className="m-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, blanditiisris id enim!</p>
+              </div>
+
+            </div>
+
+          </div>
+        </div>
+
+        <div className="flex flex-wrap container py-20 lg:px-20 md:px-0">
+          
+          <div className="lg:ml-10 lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-wrap md:items-start md:text-left mb-16 md:mb-0 items-center px-10">
+            <h1 class="sm:text-2xl lg:text-5xl mb-4 font-bold text-sky-900">We are focused on making your process a success</h1>
+            <p className="lg:text-lg md:text-base mb-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, hic voluptates quis laborum et repudiandae.</p>
+          <button class="inline-flex text-white bg-sky-500 border-0 py-3 px-6 focus:outline-none hover:bg-sky-900 hover:{transition-dely: 500ms} rounded-full font-semibold text-lg">Discover More<FaArrowRight className="mt-1 mx-1 "/></button>
+
+          </div>
+          <div className="lg:max-w-lg lg:w-full md:w-1/2 p-5">
+            <div className="">
+              <h1 className="text-3xl font-bold m-3"><FaDollarSign className="mr-2 inline-flex rounded-lg p-1 bg-indigo-400 text-amber-400"/>Save Time And Money</h1>
+                <p className="m-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, blanditiisris id enim!</p>              
+            </div>
+            <div className="">
+              <h1 className="text-3xl font-bold m-3"><FaBalanceScaleRight className="mr-2 inline-flex rounded-lg p-1 bg-indigo-400 text-black"/>Boost Sales Conversions</h1>
+                <p className="m-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, blanditiisris id enim!</p>
+            </div>
+            <div className="">
+              <h1 className="text-3xl font-bold m-3"><FaHeadset className="mr-2 inline-flex rounded-lg p-1 bg-indigo-400 text-fuchsia-700"/>Recruit Better Talent</h1>
+                <p className="m-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, blanditiisris id enim!</p>
+
+            </div>
+            <div className="">
+              <h1 className="text-3xl font-bold m-3"><FaBars className="mr-2 inline-flex rounded-lg p-1 bg-indigo-400 text-white"/>Scale Your Business</h1>
+                <p className="m-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Unde, blanditiisris id enim!</p>
+            </div>
+            </div>
+        </div>
+
+        <div className="w-full px-20 py-10 bg-sky-900">
+          <p className="text-xl text-amber-700 font-semibold uppercase">Incredible Services</p>
+          <h1 className="text-9xl font-bold mb-10">We do a business of Satisfaction</h1>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusamus alias eligendi officia tempore. Officiis, doloribus iusto! Nostrum cupiditate libero eligendi. Sit fuga laudantium illo maxime laboriosam ipsa odio, accusantium doloremque.</p>
+          <button class="my-7 inline-flex text-white bg-sky-500 border-0 py-3 px-6 focus:outline-none hover:bg-sky-900 hover:{transition-dely: 500ms} rounded-full font-semibold text-lg">Discover More<FaArrowRight className="mt-1 mx-1 "/></button>
+
+        </div>
         </>
     )
 }
