427345ec6f2aa3da628fb67cac3ca59a1915714a
abdullah-k <abdullah771.ak@gmail.com>
2023-01-28 23:56:52 +0500

    contact pages

--- my-app/pages/Contact.js
+++ my-app/pages/Contact.js
@@ -7,6 +7,8 @@ export default function Contact(){
         <>
         <div className="w-full text-center">
             <h1 className="text-5xl py-40 font-semibold text-sky-900">Contact Us</h1>
+            <h1 className="text-4xl py-40 font-semibold text-sky-900">Get In Touch</h1>
+
         </div>
         </>
     )
