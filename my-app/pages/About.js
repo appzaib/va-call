bb83f55c00b7b10cd682a27e693fa982e54f465c
abdullah-k <abdullah771.ak@gmail.com>
2023-01-29 22:40:10 +0500

    add fixes

--- my-app/pages/About.js
+++ my-app/pages/About.js
@@ -1,4 +1,5 @@
 import Link from "next/link"
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa"
 
 
 
@@ -32,25 +33,25 @@ export default function About(){
     <div class="container px-5 py-24 mx-auto">
     <div class="flex flex-col text-center w-full mb-20">
       <h1 class="text-2xl font-medium title-font mb-4 text-gray-900">OUR TEAM</h1>
-      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them.</p>
+      <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error, hic minima, culpa tenetur voluptatibus exercitationem aperiam rerum tempore ab provident vel doloremque assumenda expedita quae quod aut quidem voluptates possimus.</p>
     </div>
     <div class="flex flex-wrap -m-4">
       <div class="p-4 lg:w-1/4 md:w-1/2">
         <div class="h-full flex flex-col items-center text-center">
           <img alt="team" class="flex-shrink-0 rounded-3xl w-full h-56 object-cover object-center mb-4" src="https://avatars.githubusercontent.com/u/82768953?v=4"/>
           <div class="w-full">
-            <h2 class="title-font font-medium text-lg text-gray-900">Alper Kamu</h2>
-            <h3 class="text-gray-500 mb-3">UI Developer</h3>
+            <h2 class="title-font font-medium text-lg text-gray-900">Abdullah Khan</h2>
+            <h3 class="text-gray-500 mb-3">Front End Web Developer</h3>
             <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
             <span class="inline-flex">
               <Link href={""} class="text-gray-500">
-               <img src="" alt="img" />               
+               <FaFacebook/>              
               </Link>
               <Link href={""} class="ml-2 text-gray-500">
-               <img src="" alt="img" />
+               <FaTwitter/>
               </Link>
               <Link href={""} class="ml-2 text-gray-500">
-                <img src="" alt="img" />
+                <FaInstagram/>
               </Link>
             </span>
           </div>
@@ -60,18 +61,18 @@ export default function About(){
         <div class="h-full flex flex-col items-center text-center">
           <img alt="team" class="flex-shrink-0 rounded-3xl w-full h-56 object-cover object-center mb-4" src="https://avatars.githubusercontent.com/u/83128763?s=70&v=4"/>
           <div class="w-full">
-            <h2 class="title-font font-medium text-lg text-gray-900">Holden Caulfield</h2>
-            <h3 class="text-gray-500 mb-3">UI Developer</h3>
+            <h2 class="title-font font-medium text-lg text-gray-900">Nadia Khan</h2>
+            <h3 class="text-gray-500 mb-3">Front End Web Developer</h3>
             <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
             <span class="inline-flex">
               <Link href={""} class="text-gray-500">
-                <img src="" alt="img" />
+               <FaFacebook/>              
               </Link>
               <Link href={""} class="ml-2 text-gray-500">
-                <img src="" alt="img" />
+                <FaTwitter/>
               </Link>
               <Link href={""} class="ml-2 text-gray-500">
-                <img src="" alt="img" />
+                <FaInstagram/>
               </Link>
             </span>
           </div>
@@ -81,21 +82,19 @@ export default function About(){
         <div class="h-full flex flex-col items-center text-center">
           <img alt="team" class="flex-shrink-0 rounded-3xl w-full h-56 object-cover object-center mb-4" src="https://avatars.githubusercontent.com/u/44477943?s=70&v=4"/>
           <div class="w-full">
-            <h2 class="title-font font-medium text-lg text-gray-900">Atticus Finch</h2>
-            <h3 class="text-gray-500 mb-3">UI Developer</h3>
+            <h2 class="title-font font-medium text-lg text-gray-900">Shoaib khan</h2>
+            <h3 class="text-gray-500 mb-3">Back End Web Developer</h3>
             <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
             <span class="inline-flex">
               <Link href={""} class="text-gray-500">
-               <img src="" alt="img" />
-                
+                <FaFacebook/>
               </Link>
               <Link href={""} class="ml-2 text-gray-500">
-                               <img src="" alt="img" />
+                <FaTwitter/>
 
               </Link>
               <Link href={""} class="ml-2 text-gray-500">
-               <img src="" alt="img" />
-                
+               <FaInstagram/>
               </Link>
             </span>
           </div>
@@ -105,21 +104,21 @@ export default function About(){
         <div class="h-full flex flex-col items-center text-center">
           <img alt="team" class="flex-shrink-0 rounded-3xl w-full h-56 object-cover object-center mb-4" src="https://avatars.githubusercontent.com/u/28682772?v=4"/>
           <div class="w-full">
-            <h2 class="title-font font-medium text-lg text-gray-900">Henry Letham</h2>
-            <h3 class="text-gray-500 mb-3">UI Developer</h3>
+            <h2 class="title-font font-medium text-lg text-gray-900">Rana Jahanzaib</h2>
+            <h3 class="text-gray-500 mb-3">Full Stack Developer</h3>
             <p class="mb-4">DIY tote bag drinking vinegar cronut adaptogen squid fanny pack vaporware.</p>
             <span class="inline-flex">
               <Link href={""} class="text-gray-500">
-               <img src="" alt="img" />
+                <FaFacebook/>
+
               
               </Link>
               <Link href={""} class="ml-2 text-gray-500">
-               <img src="" alt="img" />
-                
+               <FaTwitter/>
+               
               </Link>
               <Link href={""} class="ml-2 text-gray-500">
-               <img src="" alt="img" />
-                
+               <FaInstagram/>                               
               </Link>
             </span>
           </div>
@@ -129,7 +128,10 @@ export default function About(){
   </div>
     </div> 
 
-
+            <div className="bg-red-500 h-max w-full text-center py-20">
+            <h1 className="font-bold py-20 text-4xl text-center text-sky-900">Have any projects in your minds? <br /> Lets talk about it.</h1>
+            <button class="inline-flex text-white bg-sky-500 border-0 py-3 px-6 focus:outline-none hover:bg-sky-900 hover:{transition-dely: 500ms} rounded-full font-semibold text-lg ">Register Now</button>
+            </div>
         </div>
     )
 }
